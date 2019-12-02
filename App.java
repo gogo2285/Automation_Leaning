218bc5728c885c3a5b67a7d20c7be5750259872f
gogo2285 <56085229+gogo2285@users.noreply.github.com>
2019-12-02 13:49:37 +0000

    JUNIT
    JUINT

---
+++ App.java
@@ -0,0 +1,13 @@
+package Batch2.Batch2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
