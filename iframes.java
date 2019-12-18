b4b96fb94b357bde8bfeae47a880ae05768f15cb
gogo2285 <56085229+gogo2285@users.noreply.github.com>
2019-12-18 16:18:12 +0000

    Update iframes.java

--- iframes.java
+++ iframes.java
@@ -1,6 +1,4 @@
-package Selenium;
-
-import java.util.concurrent.TimeUnit;
+package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,19 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class iframes {
 
-	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "D:\\java\\GB\\Selenium_Practise\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver(); //launch url	
-		driver.manage().window().maximize();
-		driver.get("http://demo.guru99.com/test/guru99home/");
-		int i = driver.findElements(By.tagName("iframe")).size();
-		System.out.println("the total no. of frames is " +i);
-		WebElement j= driver.findElement(By.xpath("//iframe[@src='https:/www.youtube.com/embed/RbSlW8jZFe8']"));
-		driver.switchTo().frame(j);
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
-		Thread.sleep(10000);
-		driver.switchTo().defaultContent();
+	
+		public static void main(String[] args) throws Exception {
+			//System.setProperty("webdriver.chrome.driver", "D:\\java\\GB\\Selenium_Practise\\Driver\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\gb152\\selenium\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver(); //launch url	
+			driver.manage().window().maximize();
+			driver.get("http://demo.guru99.com/test/guru99home/");
+			int i = driver.findElements(By.tagName("iframe")).size();
+			System.out.println("the total no. of frames is " +i);
+			WebElement j= driver.findElement(By.xpath("//*[@id=\"rt-showcase\"]/div/div[1]/div/div/div/div/div[2]/div/div/iframe"));
+			driver.switchTo().frame(j);
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+			Thread.sleep(10000);
+			driver.switchTo().defaultContent();
+			driver.quit();
+			
 
-}
-}
+	}
+
+	}
+
+
